0b1c9fa7854cb91dde87cd530940533382c1741b
Paulo <psergio.farias@gmail.com>
2021-05-24 22:55:55 -0400

    First commit, create project, add ignore
    Signed-off-by: Paulo <psergio.farias@gmail.com>

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] private int level;
+    [SerializeField] private int experience;
+    [SerializeField] private int xpToLevelUp;
+    
+    public int GetLevel
+    {
+        get => experience / xpToLevelUp;
+    }
+}
