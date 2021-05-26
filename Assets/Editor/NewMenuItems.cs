2f789d1df96640f0146e34b6c48e9e122d5f009b
Paulo <psergio.farias@gmail.com>
2021-05-25 22:52:27 -0400

    Add menuitem / Add new submenu item
    Signed-off-by: Paulo <psergio.farias@gmail.com>

---
+++ Assets/Editor/NewMenuItems.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    
+    public class NewMenuItems
+    {
+        [MenuItem("Window/Criar novo objeto")]
+        private static void CreateNewObject()
+        {
+            Debug.Log("Criando um novo objeto");
+        }
+
+        [MenuItem("Tools/SubMenu/Opções")]
+        private static void TestingSubMenu()
+        {
+            Debug.Log("Teste de um submenu");
+        }
+    }
+}
